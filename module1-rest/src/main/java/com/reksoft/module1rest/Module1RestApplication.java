4a418a1b7013204ae49dc1697566f4d9afa49910
Maksim Ganin <m.ganin@bestdoctor.ru>
2025-02-05 17:24:04 +0400

    init

---
+++ module1-rest/src/main/java/com/reksoft/module1rest/Module1RestApplication.java
@@ -0,0 +1,13 @@
+package com.reksoft.module1rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1RestApplication.class, args);
+	}
+
+}
